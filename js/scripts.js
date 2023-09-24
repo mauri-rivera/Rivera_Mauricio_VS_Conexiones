4cf94ddcdadfaea0062ef88fc3a5b711a763ebd9
mauri-rivera <mauri.rivera@outlook.com>
2023-09-24 17:39:40 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,42 @@
 
 function editarNombre(elemento) {
     let identificador = elemento.id;
-    console.log(identificador);
     let enlaceId = document.querySelector(`#${identificador}`);
-    console.log(enlaceId);
     let ubicacionNombre = enlaceId.closest(".bloqueParteDos");
-    console.log(ubicacionNombre);
     let nombrePerfil = ubicacionNombre.querySelector("h1");
-    console.log(nombrePerfil);
     let nombre = nombrePerfil.innerHTML;
-    console.log(nombre);
+
+    let nuevoNombre = prompt("¿Qué nombre de perfil quiere actualizarlo?:", nombre);
+
+    if (nuevoNombre == null || nuevoNombre == "") {
+        nombrePerfil.innerHTML = nombre;
+    } 
+    else {
+        nombrePerfil.innerHTML = nuevoNombre;
+    }
+}
+
+function evaluarSolicitud(elemento) {
+    let identificador = elemento.id;
+    let estadoSolicitud = identificador.substring(0, 1);
+    let enlaceUsuario = document.querySelector(`#${identificador}`);
+    let ubicacionUsuario = enlaceUsuario.closest(".bloqueFilaUsuario");
+    let bloqueSolicitud = enlaceUsuario.closest("#bloqueSolicitud");
+    let cantidadSolicitud = bloqueSolicitud.querySelector("h3").innerHTML;
+
+    if (estadoSolicitud === "a") {
+        let estadoConexion = document.querySelector("#bloqueConexion");
+        let cantidadConexion = estadoConexion.querySelector("h3").innerHTML;
+
+        ubicacionUsuario.remove();
+        cantidadSolicitud--;
+        cantidadConexion++;
+        bloqueSolicitud.querySelector("h3").innerHTML = cantidadSolicitud; 
+        estadoConexion.querySelector("h3").innerHTML = cantidadConexion; 
+    }
+    else{
+        ubicacionUsuario.remove();
+        cantidadSolicitud--;
+        bloqueSolicitud.querySelector("h3").innerHTML = cantidadSolicitud; 
+    }
 }
